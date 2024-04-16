a76ba3540f57cb18f7e991ac03be9bdbf0287b6e
Egie Santos <106503294+egiesantosdev@users.noreply.github.com>
2024-04-16 10:20:57 +0800

    Chat Drawer

---
+++ app/Views/partials/chatDrawer.php
@@ -0,0 +1,3 @@
+<div id="chat-drawer" class="bg-white" data-kt-drawer="true" data-kt-drawer-activate="true" data-kt-drawer-toggle="#chat-drawer-btn" data-kt-drawer-close="#kt_drawer_example_basic_close" data-kt-drawer-width="500px">
+    
+</div>
